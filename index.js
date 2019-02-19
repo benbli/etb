929a716245bf0cfb162acc79f253fa472e8ccbd0
Ben Li <benbli@gmail.com>
2019-02-19 11:51:42 -0800

    adding lighterhtml

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('loading')
+const { render, html, svg } = lighterhtml;
+
+const button = name => html`
+  <div class="clas">does this nice ${name}</div>
+`;
+
+document.body.append(button('now wwww'));
