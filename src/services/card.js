7aab4db9c7d2d30c46adb15b7c4537862de9e3b7
Ben Li <benbli@gmail.com>
2019-06-02 19:37:46 -0700

    Change to WAR

--- src/services/card.js
+++ src/services/card.js
@@ -1,6 +1,6 @@
 async function getCards() {
   return await fetch(
-    'https://api.magicthegathering.io/v1/cards?supertypes=legendary&set=RNA'
+    'https://api.magicthegathering.io/v1/cards?supertypes=legendary&set=WAR'
   )
     .then(response => response.json())
     .catch(error => console.log(error));
