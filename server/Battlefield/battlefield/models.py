e8b11db72ad56343ad8a2778cc5318f1c9362930
Tomred <Tomred87@hotmail.com>
2019-06-24 18:10:58 -0700

    corrected models

--- server/Battlefield/battlefield/models.py
+++ server/Battlefield/battlefield/models.py
@@ -1,33 +1,7 @@
 from datetime import datetime
 from battlefield import db
 
-
 class Player(db.Model):
-<<<<<<< HEAD
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(15), nullable=False)
-    last_name = db.Column(db.String(15), nullable=False)
-    password = db.Column(db.String(60), unique=True, nullable=False)
-    time_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    #leagues = db.relationship('League', backref='leagues', lazy=True)
-    #tournaments = db.relationship('Tournament', backref='tournaments', lazy=True)
-    #matches = db.relationship('Game', backref='matches', lazy=True)
-
-    def __repr__(self):
-        return f"Player('{self.id}', '{self.first_name}', '{self.last_name}', '{self.password}')"
-
-
-class League(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-
-class Tournament(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-
-class Game(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-=======
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(25), nullable=False)
 	password = db.Column(db.String(60), unique=True, nullable=False)
@@ -64,5 +38,4 @@ class Match(db.Model):
 	p1_game_wins = db.Column(db.Integer, nullable=False)
 	p2_game_wins = db.Column(db.Integer, nullable=False)
 	tournamentId = db.Column(db.Integer, nullable=False)
-	time_played = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
->>>>>>> dev
+time_played = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
