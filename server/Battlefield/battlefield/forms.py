019e45f37478a69ed063b3fae0596450ed2e31f6
chenghsiaoc <chenghsiao.cheng@gmail.com>
2019-06-25 19:55:26 -0700

    Create routes for Scoreborad, and Tournament creator

--- server/Battlefield/battlefield/forms.py
+++ server/Battlefield/battlefield/forms.py
@@ -2,5 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
-class DraftForm(FlaskForm):
-	player = StringField('player', validators=[Length(min=3, max=20)])
+
+class TournamentForm(FlaskForm):
+    name = StringField(
+
+        'name', validators=[DataRequired(), Length(min=3, max=30)]
+
+    )
+
+    submit = SubmitField('create')
--- server/Battlefield/battlefield/models.py
+++ server/Battlefield/battlefield/models.py
@@ -1,41 +1,41 @@
 from datetime import datetime
 from battlefield import db
 
-class Player(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(25), nullable=False)
-	password = db.Column(db.String(60), unique=True, nullable=False)
-	time_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-	#leagues = db.relationship('League', backref='leagues', lazy=True)
-	#tournaments = db.relationship('Tournament', backref='tournaments', lazy=True)
-	#matches_won = db.relationship('Match', backref='won', lazy=True, foreign_keys='Match.winner')
-	#matches_lost = db.relationship('Match', backref='lost', lazy=True, foreign_keys='Match.loser')
 
-	def __repr__(self):
-		return f"Player('{self.id}', '{self.name}', '{self.password}')"
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+    password = db.Column(db.String(60), unique=True, nullable=False)
+    time_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    #leagues = db.relationship('League', backref='leagues', lazy=True)
+    #tournaments = db.relationship('Tournament', backref='tournaments', lazy=True)
+    #matches_won = db.relationship('Match', backref='won', lazy=True, foreign_keys='Match.winner')
+    #matches_lost = db.relationship('Match', backref='lost', lazy=True, foreign_keys='Match.loser')
+
+    def __repr__(self):
+        return f"Player('{self.id}', '{self.name}', '{self.password}')"
 
 
 class Tournament(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(30), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
 
 
 class Scoreboard(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	player = db.Column(db.Integer, nullable=False)
-	wins = db.Column(db.Integer, nullable=False, default=0)
-	losses = db.Column(db.Integer, nullable=False, default=0)
-	draws = db.Column(db.Integer, nullable=False, default=0)
-	tiebreak = db.Column(db.Float, nullable=False, default=0)
-	tournamentId = db.Column(db.Integer, nullable=False)
-
+    id = db.Column(db.Integer, primary_key=True)
+    player = db.Column(db.Integer, nullable=False)
+    wins = db.Column(db.Integer, nullable=False, default=0)
+    losses = db.Column(db.Integer, nullable=False, default=0)
+    draws = db.Column(db.Integer, nullable=False, default=0)
+    tiebreak = db.Column(db.Float, nullable=False, default=0)
+    tournamentId = db.Column(db.Integer, nullable=False)
 
 
 class Match(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	player1 = db.Column(db.Integer, nullable=False)
-	player2 = db.Column(db.Integer, nullable=False)
-	p1_game_wins = db.Column(db.Integer, nullable=False)
-	p2_game_wins = db.Column(db.Integer, nullable=False)
-	tournamentId = db.Column(db.Integer, nullable=False)
-	time_played = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    player1 = db.Column(db.Integer, nullable=False)
+    player2 = db.Column(db.Integer, nullable=False)
+    p1_game_wins = db.Column(db.Integer, nullable=False)
+    p2_game_wins = db.Column(db.Integer, nullable=False)
+    tournamentId = db.Column(db.Integer, nullable=False)
+    time_played = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
