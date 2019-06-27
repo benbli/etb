037647a6d6732a949e68186e1e3a76901d95fdba
chenghsiaoc <chenghsiao.cheng@gmail.com>
2019-06-26 19:38:27 -0700

    create an option to set up # of players for a tournament, and randomized players

--- server/Battlefield/battlefield/forms.py
+++ server/Battlefield/battlefield/forms.py
@@ -1,16 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 
-
 class RegisterForm(FlaskForm):
-	username = StringField('username', validators=[DataRequired(), Length(min=3, max=20)])
-	password = PasswordField('password', validators=[DataRequired(), Length(min=8, max=80)])
-	submit = SubmitField('Register')
-
+    username = StringField('username', validators=[DataRequired(), Length(min=3, max=20)])
+    password = PasswordField('password', validators=[DataRequired(), Length(min=8, max=80)])
+    submit = SubmitField('Register')
 
 
 class TournamentForm(FlaskForm):
     name = StringField('name', validators=[DataRequired(), Length(min=3, max=30)])
-    submit = SubmitField('create')
+    numofplayers = IntegerField('numofplayers', validators=[DataRequired()])
+    submit = SubmitField('Create')
--- server/Battlefield/battlefield/route_functions.py
+++ server/Battlefield/battlefield/route_functions.py
@@ -2,29 +2,48 @@ from flask import render_template, request, jsonify, redirect, url_for
 from battlefield import app, db, bcrypt
 from battlefield.models import Player, Scoreboard, Match, Tournament
 from battlefield.forms import TournamentForm, RegisterForm
-
+import random
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-	form = RegisterForm()
-	if form.validate_on_submit():
-		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-		new_player = Player(name=form.username.data, password=hashed_password)
-		db.session.add(new_player)
-		db.session.commit()
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_player = Player(name=form.username.data, password=hashed_password)
+        db.session.add(new_player)
+        db.session.commit()
 
-		return redirect(url_for('get_all_players'))
-	return render_template('register.html', form=form)
-
+        return redirect(url_for('get_all_players'))
+    return render_template('register.html', form=form)
 
 
 @app.route("/create_tournament", methods=['GET', 'POST'])
 def create_tournament():
     form = TournamentForm()
+    numofplayers = form.numofplayers.data
+
     if form.validate_on_submit():
         new_tournament = Tournament(name=form.name.data)
         db.session.add(new_tournament)
         db.session.commit()
-        return 'success'
+
+        players = Player.query.filter(Player.id <= numofplayers)
+        player_list = []
+
+        for player in players:
+            player_list.append(player.name)
+
+        random.shuffle(player_list)
+
+        return jsonify(player_list)
+
     return render_template('create_tournament.html', form=form)
+
+
+# if players:
+    #     for player in players:
+    #         player_data = {}
+    #         player_data['player'] = player.name
+    #         outputs.append(player_data)
+    #         return jsonify(player=outputs)
