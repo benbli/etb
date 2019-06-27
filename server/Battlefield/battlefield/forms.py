7d2dc1393b52b259882cc2385c7d9d7de2da136f
chenghsiaoc <chenghsiao.cheng@gmail.com>
2019-06-27 16:23:05 -0700

    add seating route and on progress to add a button to generate matches

--- server/Battlefield/battlefield/forms.py
+++ server/Battlefield/battlefield/forms.py
@@ -3,22 +3,23 @@ from wtforms import StringField, SubmitField, PasswordField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 
-
 class RegisterForm(FlaskForm):
-	username = StringField('username', validators=[DataRequired(), Length(min=3, max=20)])
-	password = PasswordField('password', validators=[DataRequired(), Length(min=8, max=80)])
-	submit = SubmitField('Register')
-
+    username = StringField('username', validators=[DataRequired(), Length(min=3, max=20)])
+    password = PasswordField('password', validators=[DataRequired(), Length(min=8, max=80)])
+    submit = SubmitField('Register')
 
 
 class TournamentForm(FlaskForm):
     name = StringField('name', validators=[DataRequired(), Length(min=3, max=30)])
     numofplayers = IntegerField('numofplayers', validators=[DataRequired()])
-    submit = SubmitField('create')
+    submit = SubmitField('Create')
 
+
+class SeatingForm(FlaskForm):
+    submit = SubmitField('Generate')
 
 
 class StatisticsForm(FlaskForm):
-	user_id = IntegerField('user_id', validators=[])
-	username = StringField('username', validators=[Length(max=20)])
-	submit = SubmitField('Get Stats')
+    user_id = IntegerField('user_id', validators=[])
+    username = StringField('username', validators=[Length(max=20)])
+    submit = SubmitField('Get Stats')
