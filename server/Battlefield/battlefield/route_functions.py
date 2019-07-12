2560c093b2e3a8b494c507d28a354a584ff5c3e1
chenghsiaoc <chenghsiao.cheng@gmail.com>
2019-07-12 16:55:09 -0700

    Add comments for all the codes in route_functions

--- server/Battlefield/battlefield/route_functions.py
+++ server/Battlefield/battlefield/route_functions.py
@@ -24,19 +24,20 @@ def create_tournament():
     form = TournamentForm()
     numofplayers = form.numofplayers.data
 
+    # WHEN SUBMITTED, ADDS NEW ENTRY TO TOURNAMENTS DB
+    # ADDS NEW ENTRY TO SCOREBORAD DB PER PLAYER
     if form.validate_on_submit():
         new_tournament = Tournament(name=form.name.data)
         db.session.add(new_tournament)
-        # db.session.commit()
 
+        # LINE BELOW PULLS THE FIRST X PLAYERS, WHERE X EQUALS TO THE NUMBERS OF PLAYERS ENTERED. NEEDS TO BE FIXED.
         players = Player.query.filter(Player.id <= numofplayers)
-        # player_list = {}
         for player in players:
             new_scoreboard = Scoreboard(player=player.id, tournamentId=new_tournament.id)
             db.session.add(new_scoreboard)
         db.session.commit()
-        #     player_list.update({player.id: player.name})
-        # session['player_list'] = player_list
+
+        # NOT CERTAIN IF SESSION IS SECURE
         session['tournamentId'] = new_tournament.id
 
         return redirect(url_for('seating'))
@@ -48,13 +49,26 @@ def create_tournament():
 def seating():
 
     form = SeatingForm()
-    # tournamentId = session.get('tournamentId')
-    tournamentId = 200
-    scoreboards = Scoreboard.query.filter(Scoreboard.tournamentId == tournamentId)
+    # BELOW THE CHANGE OF THE TOURNAMENT ID IS JUST FOR TESTING PURPOSES
+    tournamentId = session.get('tournamentId')
+    # tournamentId = 200
 
-    scoreboard_dict = {}
-    player_id_list = []
-    max_points = 0
+    ##################################################
+    # GENERATE LIST OF PLAYERS SORTED BY MOST POINTS #
+    ##################################################
+    scoreboard_dict = {}       # DICT KEY: PLAYER ID; VALUE: POINTS
+    player_id_list = []        # LIST OF PLAYER IDs
+    max_points = 0             # GREATEST POINTS VALUE AMONG PLAYERS
+    points_lists = []          # LIST OF LISTS, WHERE EACH SUBLIST CONTAINS ALL PLAYERS OF A CERTAIN POINT VALUE. SOME SUBLISTS MIGHT BE EMPTY.
+    points_combined_list = []  # A SINGLE LIST OF ALL THE ELEMENTS FROM SUBLISTS IN points_lists. MAINTAINS ORDER BY POINT VALUE. NOTE THAT THIS IS A LIST OF PLAYER IDs, NOT A LIST OF LISTS.
+    player_dict = {}           # DICT KEY: PLAYER ID; VALUE: PLAYER NAME. THIS IS FOR FRONT END PURPOSES
+    name_list = []             # LIST OF PLAYER NAMES. THIS IS FOR FRONT END PURPOSES
+
+    scoreboards = Scoreboard.query.filter(Scoreboard.tournamentId == tournamentId)  # PULLS THE SCOREBORADS BY TOURNAMENT ID
+
+    # GRAB SCOREBOARD OF EACH PARTICIPANT IN TOURNAMENT AND CALCULATE THEIR POINTS
+    # player_id_list POPULATED (UNSORTED)
+    # max_points IS SET
     for scoreboard in scoreboards:
         points = (scoreboard.wins * 3) + (scoreboard.draws * 1)
         scoreboard_dict.update({scoreboard.player: points})
@@ -62,58 +76,61 @@ def seating():
         if points > max_points:
             max_points = points
 
-    points_lists = []
-    while max_points >= 0:
+    players = Player.query.filter(Player.id.in_(player_id_list)).all()             # PULLS PLAYERS THAT ONLY IN player_id_list
+
+    # GENERATE LISTS, WHERE EACH LIST CONTAINS ALL PLAYERS WITH A SPECIFIC POINT VALUE. EACH LIST IS SHUFFLED THEN APPENDED TO points_lists
+    temp_max_points = max_points
+    while temp_max_points >= 0:
         these_points = []
         for key, value in scoreboard_dict.items():
-            if value == max_points:
+            if value == temp_max_points:
                 these_points.append(key)
         random.shuffle(these_points)
         points_lists.append(these_points)
-        max_points -= 1
+        temp_max_points -= 1
 
-    points_combined_list = []
+    # POPULATES points_combined_list USING points_lists
     for this_list in points_lists:
         for i in this_list:
             points_combined_list.append(i)
 
-    players = Player.query.filter(Player.id.in_(player_id_list)).all()
-
-    player_dict = {}
+    # POPULATES player_dict
     for player in players:
         player_dict.update({player.id: player.name})
 
-    # key_list = []
-    # for key in player_dict:
-    #     key_list.append(key)
-
-    name_list = []
+    # POPULATES name_list USING player_dict
     for i in points_combined_list:
         name_list.append(player_dict[i])
 
     # CURRENTLY DOES NOT CHECK for EMPTY NAME or NUMBERS of PLAYERS
     if form.is_submitted():
         matches = Match.query.filter(Match.tournamentId == tournamentId)
+
+        matchup_list = []       # A LIST OF LISTS, WHERE EACH SUBLIST CONTAINS THE PLAYERS THAT WOULD BE PAIRED UP.
+        matchup_list_names = [] # A LIST OF PLAYER NAME FOR EACH MATCHUP. FOR FRONT END PURPOSES.
+
     # IF THE LAST PAIR IS A CONFLICT BUT EVERY OTHER PIARS IS OKAY THIS ALGORITHM WON'T WORK. WE'll ALSO HIT AN ERROR
-        for i in range(len(points_combined_list)):
-            if (i % 2 == 0) and (len(points_combined_list) - 1 > i):
-                counter = 2
-                for match in matches:
-                    if points_combined_list[i] == match.player1:
-                        if points_combined_list[i + 1] == match.player2:
-                            temp_player = points_combined_list[i + 1]
-                            points_combined_list[i + 1] = points_combined_list[i + counter]
-                            points_combined_list[i + counter] = temp_player
-                            counter += 1
+    # COMMENT BELOW ALGORITHM SINCE ABOVEMENTIONED ISSUE. WILL FIX THIS LATER ON.
+        # for i in range(len(points_combined_list)):
+        #     if (i % 2 == 0) and (len(points_combined_list) - 1 > i):
+        #         counter = 2
+        #         for match in matches:
+        #             if points_combined_list[i] == match.player1:
+        #                 if points_combined_list[i + 1] == match.player2:
+        #                     temp_player = points_combined_list[i + 1]
+        #                     points_combined_list[i + 1] = points_combined_list[i + counter]
+        #                     points_combined_list[i + counter] = temp_player
+        #                     counter += 1
 
-                    elif points_combined_list[i] == match.player2:
-                        if points_combined_list[i + 1] == match.player1:
-                            temp_player = points_combined_list[i + 1]
-                            points_combined_list[i + 1] = points_combined_list[i + counter]
-                            points_combined_list[i + counter] = temp_player
-                            counter += 1
+        #             elif points_combined_list[i] == match.player2:
+        #                 if points_combined_list[i + 1] == match.player1:
+        #                     temp_player = points_combined_list[i + 1]
+        #                     points_combined_list[i + 1] = points_combined_list[i + counter]
+        #                     points_combined_list[i + counter] = temp_player
+        #                     counter += 1
 
-        matchup_list = []
+
+        # POPULATES matchup_list by using points_combined_list. WHEN NUMBERS OF PLAYERS IS ODD, THE LAST PLAYER WILL PAIR WITH PLAYER ID 0 WHO DOESN'T EXIST IN THE DB, EFFECTIVELY GIVING THE LAST PLAYER A BYE.
         for player in points_combined_list:
             if points_combined_list.index(player) % 2 == 0:
                 matchup = []
@@ -124,17 +141,17 @@ def seating():
                     matchup.append(points_combined_list[points_combined_list.index(player) + 1])
                 matchup_list.append(matchup)
 
+        # COMMIT MATCH ENTRIES TO DB
         for matchup in matchup_list:
             new_match = Match(
                 player1=matchup[0], player2=matchup[1],
                 p1_game_wins=0, p2_game_wins=0,
                 tournamentId=tournamentId
             )
-        #     db.session.add(new_match)
-        # db.session.commit()
+            db.session.add(new_match)
+        db.session.commit()
 
-        matchup_list_names = []
-
+        # POPULATES matchup_list_names by matchup_names
         for matchup in matchup_list:
             matchup_names = []
             for player in matchup:
