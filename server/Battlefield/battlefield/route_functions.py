b1a179370cb49028bd27b023c17bb47bcf7f3d17
Tomred <Tomred87@hotmail.com>
2019-07-24 21:26:32 -0700

    refactoring seating

--- server/Battlefield/battlefield/route_functions.py
+++ server/Battlefield/battlefield/route_functions.py
@@ -20,6 +20,8 @@ def register():
     return render_template('register.html', form=form)
 
 
+
+
 @app.route("/create_tournament", methods=['GET', 'POST'])
 def create_tournament():
 
@@ -42,24 +44,50 @@ def create_tournament():
 
         # NOT CERTAIN IF SESSION IS SECURE
         session['tournamentId'] = new_tournament.id
-        session['roundNumber'] = -1
         return redirect(url_for('seating'))
 
     return render_template('create_tournament.html', form=form, dbplayers=dbplayers)
 
 
+
+
 @app.route("/seating", methods=['GET', 'POST'])
 def seating():
-
     form = SeatingForm()
-    # BELOW THE CHANGE OF THE TOURNAMENT ID IS JUST FOR TESTING PURPOSES
     tournamentId = session.get('tournamentId')
-    session['roundNumber'] += 1
-    # tournamentId = 200
+    session['roundNumber'] = 0
 
+    player_id_list = []
+    player_name_list = []
+
+    # PULLS THE SCOREBORADS BY TOURNAMENT ID
+    scoreboards = Scoreboard.query.filter(
+        Scoreboard.tournamentId == tournamentId)
+
+    for scoreboard in scoreboards:
+        player_id_list.append(scoreboard.player)
+
+    session['player_id_list'] = player_id_list
+    # PULLS PLAYERS THAT ONLY IN player_id_list
+    players = Player.query.filter(Player.id.in_(player_id_list)).all()
+
+    for player in players:
+        player_name_list.append(player.name)
+
+    if form.is_submitted():
+        return "got there"
+        # return redirect(url_for('rounds'))
+
+    return render_template('seating.html', player_list=player_name_list, form=form)
+
+
+
+
+@app.route("/rounds", methods=['GET', 'POST'])
+def rounds():
     ##################################################
     # GENERATE LIST OF PLAYERS SORTED BY MOST POINTS #
-    ##################################################
+    ##################################################    
     scoreboard_dict = {}       # DICT KEY: PLAYER ID; VALUE: POINTS
     player_id_list = []        # LIST OF PLAYER IDs
     max_points = 0             # GREATEST POINTS VALUE AMONG PLAYERS
@@ -67,9 +95,7 @@ def seating():
     points_lists = []
     # A SINGLE LIST OF ALL THE ELEMENTS FROM SUBLISTS IN points_lists. MAINTAINS ORDER BY POINT VALUE. NOTE THAT THIS IS A LIST OF PLAYER IDs, NOT A LIST OF LISTS.
     points_combined_list = []
-    # DICT KEY: PLAYER ID; VALUE: PLAYER NAME. THIS IS FOR FRONT END PURPOSES
-    player_dict = {}
-    name_list = []             # LIST OF PLAYER NAMES. THIS IS FOR FRONT END PURPOSES
+
 
     # PULLS THE SCOREBORADS BY TOURNAMENT ID
     scoreboards = Scoreboard.query.filter(
@@ -104,82 +130,49 @@ def seating():
         for i in this_list:
             points_combined_list.append(i)
 
-    # POPULATES player_dict
-    for player in players:
-        player_dict.update({player.id: player.name})
 
-    # POPULATES name_list USING player_dict
-    for i in points_combined_list:
-        name_list.append(player_dict[i])
 
-    # CURRENTLY DOES NOT CHECK for EMPTY NAME or NUMBERS of PLAYERS
-    if form.is_submitted():
-        # round_number = request.form.get()
-        round_number = session['roundNumber']
-        matches = Match.query.filter(Match.tournamentId == tournamentId)
+    tournamentId = session.get('tournamentId')
+    session['roundNumber'] += 1
+    round_number = session['roundNumber']
+    matches = Match.query.filter(Match.tournamentId == tournamentId)
 
-        # A LIST OF LISTS, WHERE EACH SUBLIST CONTAINS THE PLAYERS THAT WOULD BE PAIRED UP.
-        matchup_list = []
-        # A LIST OF PLAYER NAME FOR EACH MATCHUP. FOR FRONT END PURPOSES.
-        matchup_list_names = []
+    # A LIST OF LISTS, WHERE EACH SUBLIST CONTAINS THE PLAYERS THAT WOULD BE PAIRED UP.
+    matchup_list = []
+    # A LIST OF PLAYER NAME FOR EACH MATCHUP. FOR FRONT END PURPOSES.
+    matchup_list_names = []
 
-    # IF THE LAST PAIR IS A CONFLICT BUT EVERY OTHER PIARS IS OKAY THIS ALGORITHM WON'T WORK. WE'll ALSO HIT AN ERROR
-    # COMMENT BELOW ALGORITHM SINCE ABOVEMENTIONED ISSUE. WILL FIX THIS LATER ON.
-        # for i in range(len(points_combined_list)):
-        #     if (i % 2 == 0) and (len(points_combined_list) - 1 > i):
-        #         counter = 2
-        #         for match in matches:
-        #             if points_combined_list[i] == match.player1:
-        #                 if points_combined_list[i + 1] == match.player2:
-        #                     temp_player = points_combined_list[i + 1]
-        #                     points_combined_list[i + 1] = points_combined_list[i + counter]
-        #                     points_combined_list[i + counter] = temp_player
-        #                     counter += 1
-
-        #             elif points_combined_list[i] == match.player2:
-        #                 if points_combined_list[i + 1] == match.player1:
-        #                     temp_player = points_combined_list[i + 1]
-        #                     points_combined_list[i + 1] = points_combined_list[i + counter]
-        #                     points_combined_list[i + counter] = temp_player
-        #                     counter += 1
-
-        # POPULATES matchup_list by using points_combined_list. WHEN NUMBERS OF PLAYERS IS ODD, THE LAST PLAYER WILL PAIR WITH PLAYER ID 0 WHO DOESN'T EXIST IN THE DB, EFFECTIVELY GIVING THE LAST PLAYER A BYE.
-        for player in points_combined_list:
-            if points_combined_list.index(player) % 2 == 0:
-                matchup = []
+    for player in points_combined_list:
+        if points_combined_list.index(player) % 2 == 0:
+            matchup = []
+            matchup.append(
+                points_combined_list[points_combined_list.index(player)])
+            if len(points_combined_list) - points_combined_list.index(player) == 1:
+                matchup.append(0)
+            else:
                 matchup.append(
-                    points_combined_list[points_combined_list.index(player)])
-                if len(points_combined_list) - points_combined_list.index(player) == 1:
-                    matchup.append(0)
-                else:
-                    matchup.append(
-                        points_combined_list[points_combined_list.index(player) + 1])
-                matchup_list.append(matchup)
+                    points_combined_list[points_combined_list.index(player) + 1])
+            matchup_list.append(matchup)
 
-        # COMMIT MATCH ENTRIES TO DB
-        for matchup in matchup_list:
-            new_match = Match(
-                player1=matchup[0], player2=matchup[1],
-                p1_game_wins=0, p2_game_wins=0,
-                tournamentId=tournamentId
-            )
-            db.session.add(new_match)
-        db.session.commit()
+    # COMMIT MATCH ENTRIES TO DB
+    for matchup in matchup_list:
+        new_match = Match(
+            player1=matchup[0], player2=matchup[1],
+            p1_game_wins=0, p2_game_wins=0,
+            tournamentId=tournamentId
+        )
+        db.session.add(new_match)
+    db.session.commit()
 
-        # POPULATES matchup_list_names by matchup_names
-        for matchup in matchup_list:
-            matchup_names = []
-            for player in matchup:
-                if player != 0:
-                    matchup_names.append(player_dict[player])
-                else:
-                    matchup_names.append('BYE')
-            matchup_list_names.append(matchup_names)
-
-        return render_template('seating.html', player_list=matchup_list_names, round_number=round_number, form=form)
-
-    return render_template('seating.html', player_list=name_list, form=form)
-
+    # POPULATES matchup_list_names by matchup_names
+    for matchup in matchup_list:
+        matchup_names = []
+        for player in matchup:
+            if player != 0:
+                matchup_names.append(player_dict[player])
+            else:
+                matchup_names.append('BYE')
+        matchup_list_names.append(matchup_names)
 
 @app.route("/view_stats", methods=['GET', 'POST'])
 def view_stats():
